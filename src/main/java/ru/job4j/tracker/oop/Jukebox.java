892b8688b941b75863b8e4c222047494d69f2d58
mvg79 <mvg79@mail.ru>
2022-04-10 17:57:38 +0400

    Вызов метода с аргументами. [#166086]

---
+++ src/main/java/ru/job4j/tracker/oop/Jukebox.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker.oop;
+
+public class Jukebox {
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        } else if (position == 2) {
+            System.out.println("Спокойной ночи");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Jukebox song = new Jukebox();
+        int position = 1;
+        song.music(position);
+        position = 2;
+        song.music(position);
+        position = 3;
+        song.music(position);
+    }
+}
