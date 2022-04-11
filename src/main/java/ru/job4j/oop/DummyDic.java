0515f4af7c4ec35c97fca122024eb147cd4a7962
mvg79 <mvg79@mail.ru>
2022-04-11 13:38:30 +0400

    Изменение вызова метода с возвращаемым типом. [#167387]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -2,13 +2,6 @@ package ru.job4j.oop;
 
 public class DummyDic {
     public String engToRus(String eng) {
-        String dic = "Неизвестное слово";
-        return dic + eng;
-    }
-
-    public static void main(String[] args) {
-        DummyDic dic = new DummyDic();
-        String sent = dic.engToRus("Еще слово");
-        System.out.println(sent);
+        return "Неизвестное слово" + eng;
     }
 }
