3fe331892b02933c61ed3b1deae4789a5a4f2d91
mvg79 <mvg79@mail.ru>
2022-04-15 15:33:00 +0400

    Аннотация @Override [#174891]

---
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class HtmlReport {
+
+    @Override
+    public String genreate(String name, String body) {
+        return "<h1>" + name + "</h1>"
+                + "<br/>"
+                + "<span>" + body + "</span>";
+    }
+}
