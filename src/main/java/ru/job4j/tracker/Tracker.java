6b8b988371c796a82e5ff46b90c13d1010d1b69c
mvg79 <mvg79@mail.ru>
2022-04-18 12:45:18 +0400

    Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,30 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        int rSize = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index] != null) {
+                rsl[rSize] = items[index];
+                rSize++;
+            }
+        }
+        return Arrays.copyOf(rsl, rSize);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int rSize = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getName().equals(key)) {
+                rsl[rSize] = items[index];
+                rSize++;
+            }
+        }
+        return Arrays.copyOf(rsl, rSize);
     }
 
     public Item findById(int id) {
