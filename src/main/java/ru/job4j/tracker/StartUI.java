71a5026fa228de5b4c34085f5ce0d768335ebbae
mvg79 <mvg79@mail.ru>
2022-04-19 19:51:46 +0400

    toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,7 +7,6 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(formatter));
+        System.out.println(item);
     }
 }
