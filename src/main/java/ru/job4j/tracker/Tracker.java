a8cb4482e98c71c0ea07c7fc879cfd290fde783b
mvg79 <mvg79@mail.ru>
2022-04-19 12:55:12 +0400

    Метод замены заявки. Tracker.replace [#211748 #290348]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -58,9 +58,8 @@ public class Tracker {
             item.setId(id);
             items[index] = item;
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     public boolean delete(int id) {
