f26b84ef60ab0c944b1475836ae96206ba6d2ce8
mvg79 <mvg79@mail.ru>
2022-04-27 12:39:46 +0400

    Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (key == value[i]) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("ElementNotFoundException");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] value = new String[] {"Winter", "Spring", "Summer", "Autumn"};
+        try {
+            System.out.println(indexOf(value, "Spr"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
