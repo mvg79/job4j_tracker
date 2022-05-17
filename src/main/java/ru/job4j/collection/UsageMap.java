0fcdfc06edeb306d9813477152310df7e7cf6ec7
mvg79 <mvg79@mail.ru>
2022-05-17 09:30:45 +0400

    Map, HashMap [#212652 #301485]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,7 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("mvg79@mail.ru", "Mikhail Gorshkov");
-        for (String key: map.keySet()) {
+        for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
         }
